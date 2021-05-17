7269f18a4c08fd11629deb0998b6ea81f801f79e
Albin-Joseph <60734163+albin-joseph-wiznet@users.noreply.github.com>
2021-05-17 09:22:02 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,7 +5,8 @@ module.exports = {
 //   url: 'https://your-docusaurus-test-site.com',
   url:'wiznetcloudtest.github.io',
   baseUrl: '/',
-  onBrokenLinks: 'throw',
+//   onBrokenLinks: 'throw',
+    onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
   organizationName: 'facebook', // Usually your GitHub org/user name.
