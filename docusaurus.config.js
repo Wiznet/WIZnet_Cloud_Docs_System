ee755602b2420b35a1ebae58f3d5de06ee1f81c4
Albin-Joseph <60734163+albin-joseph-wiznet@users.noreply.github.com>
2021-05-20 13:40:19 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,33 +1,77 @@
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
+  title: 'WIZnet Cloud Documentation System',
+  tagline: 'Welcome to new WIZnet Cloud Documentation System!',
+//   url: 'https://your-docusaurus-test-site.com',
+//   url:'https://wiznetcloudtest.github.io',
+    url:'https://wiznet.github.io/WIZnet_Cloud_Docs_System',
+//   baseUrl: '/',
+//     baseUrl: '/WIZnet_Cloud_Docs_System/',
+      baseUrl: '/WIZnet_Cloud_Docs_System/',
+//   onBrokenLinks: 'throw',
+    onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  favicon: 'img/wizfavicon.ico',
+  organizationName: 'Wiznet', // Usually your GitHub org/user name.
+  projectName: 'WIZnet_Cloud_Docs_System', // Usually your repo name.
   themeConfig: {
-    navbar: {
-      title: 'My Site',
-      logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
+    colorMode: {
+      defaultMode: 'light',
+      disableSwitch: false,
+      respectPrefersColorScheme: false,
+      switchConfig: {
+        darkIcon: '\u{26c8}',
+        lightIcon: '\u{1f324}',
       },
-      items: [
+    },    
+    navbar: {
+      title: 'Wiznet Cloud Documentation System !!!',
+      logo: {
+        alt: 'Wiznet Logo',
+        src: 'img/wiznet_logo.svg',
+        srcDark: 'img/wiznet_logo-white.svg',
+//         position: 'right'
+      },
+ items: [
         {
-          type: 'doc',
-          docId: 'intro',
-          position: 'left',
-          label: 'Tutorial',
+          label: 'DOCUMENTS!',
+          position: 'left', // or 'left'
+          items: [
+            // {
+            //   label: 'HOME',
+            //   to: '/',
+            // },
+            {
+              label: 'Introduction ➤',
+//               to: '/docs/intro',
+//               to: 'intro',              
+              to: '/',
+            },
+//             {
+//               label: 'Products',
+//               to: '/',
+//             },
+            {
+              label: 'Design',
+              to: '/',
+            },
+            {
+              label: 'Products',
+              to: '/',
+            },
+            // ... more items
+          ],
         },
-        {to: '/blog', label: 'Blog', position: 'left'},
+         // {to: 'docs/introduction', label: 'INTRODUCTION TO WIZNET DOCUMENTS ➤', position: 'right'},
+         // {to: 'docs/download', label: 'Download', position: 'right'},
+         {href: 'https://forum.wiznet.io/', label: 'Forum', position: 'left'},
+         {href: 'https://www.wiznet.io/', label: 'WIZnet', position: 'left'},
+
+        {to: 'blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/Wiznet/WIZnet_Cloud_Docs_System',
           label: 'GitHub',
-          position: 'right',
+          position: 'left',
         },
       ],
     },
@@ -38,8 +82,10 @@ module.exports = {
           title: 'Docs',
           items: [
             {
-              label: 'Tutorial',
-              to: '/docs/intro',
+              label: 'Documents',
+//               to: '/docs/intro',
+//               to: 'intro',
+              to: '/',              
             },
           ],
         },
@@ -77,12 +123,13 @@ module.exports = {
       copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
     },
   },
+  plugins: ['./PreventTrailingSlash/PreventTrailingSlashPlugin'],
   presets: [
     [
       '@docusaurus/preset-classic',
       {
         docs: {
-          routeBasePath:'/',
+          routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
@@ -94,6 +141,9 @@ module.exports = {
           editUrl:
             'https://github.com/facebook/docusaurus/edit/master/website/blog/',
         },
+//         sitemap:{
+//         trailingSlash: false
+//         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
